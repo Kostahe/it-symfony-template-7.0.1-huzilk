556159f930aff811fd7f248ad35b0abd5f38cad7
huzilk <huzilk@ssemi.cz>
2024-04-30 09:35:23 +0200

    Distant vyuka 2

---
+++ web/src/Controller/HuzilController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Controller;
+
+use App\Model\Strings;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class HuzilController extends BaseController
+{
+
+    /**
+     * @return Response
+     */
+    #[Route('/huzil', name: 'default')]
+    public function default(): Response
+    {
+        $title = "Homepage default";
+        return $this->render("Huzil/default.html.twig", ["title" => $title]);
+    }
+
+    #[Route('/huzil/detail', name: 'detail')]
+    public function detail(): Response
+    {
+        $title = "Homepage detail";
+        return $this->render("Huzil/detail.html.twig", ["title" => $title]);
+    }
+
+    #[Route('/huzil/delete', name: 'delete')]
+    public function delete(): Response
+    {
+        $title = "Homepage delete";
+        return $this->render("Huzil/delete.html.twig", ["title" => $title]);
+    }
+
+    #[Route('/huzil/edit', name: 'edit')]
+    public function edit(): Response
+    {
+        $title = "Homepage delete";
+        return $this->render("Huzil/edit.html.twig", ["title" => $title]);
+    }
+}
